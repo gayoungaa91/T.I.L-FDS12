e1ea8ca815505e0994472797e3f9f272a87528b1
gayoungaa91 <gayoungaa91@gmail.com>
2020-01-29 22:20:19 +0900

    add/folder hello-react

--- study/React/learning-react-book/exercise/hello-react/src/App.js
+++ study/React/learning-react-book/exercise/hello-react/src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = '리액트';
+  const num = 0;
+return num && <div>내용</div>;
 }
 
 export default App;
