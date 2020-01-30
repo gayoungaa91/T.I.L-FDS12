680872f6f4ae051ecec9c30d407d2b3e6e90b64b
gayoungaa91 <gayoungaa91@gmail.com>
2020-01-30 20:32:22 +0900

    add/folder hello-react

--- study/React/learning-react-book/exercise/hello-react/src/App.js
+++ study/React/learning-react-book/exercise/hello-react/src/App.js
@@ -3,9 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = '리액트';
-  const num = 0;
-return num && <div>내용</div>;
+  const name = '우선순위 1순위는 사랑해 이가영';
+  return (
+    <div
+
+    style = {{
+      backgroundColor: 'black',
+      color: 'pink',
+      padding: 16,
+      fontSize:100,
+      textAlign: 'center'
+    }}
+    >
+      {name}
+      </div>
+  );
 }
 
 export default App;
