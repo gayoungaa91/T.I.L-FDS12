00db01615c077155458af030052887d53754b9a8
gayoungaa91 <gayoungaa91@gmail.com>
2020-01-30 20:56:09 +0900

    react/bk/summary03

--- study/React/learning-react-book/exercise/hello-react/src/App.js
+++ study/React/learning-react-book/exercise/hello-react/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 
 function App() {
   const name = '우선순위 1순위는 사랑해 이가영';
-  return (
-    <div
-
-    style = {{
-      backgroundColor: 'black',
-      color: 'pink',
-      padding: 16,
-      fontSize:100,
-      textAlign: 'center'
-    }}
-    >
-      {name}
-      </div>
-  );
+  return 
+  <h1>
+  { name }
+  <h2>밥은 먹고 다니냐</h2>
+  </h1>;
 }
-
 export default App;
