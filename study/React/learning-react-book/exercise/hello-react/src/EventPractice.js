25cb4a0834c061d028dab2c5032bd84844ceef28
gayoungaa91 <gayoungaa91@gmail.com>
2020-01-31 17:47:58 +0900

    react/bk/ex/add04

---
+++ study/React/learning-react-book/exercise/hello-react/src/EventPractice.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default EventPractice;
