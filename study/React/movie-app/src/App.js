123f3f4a6a8e00d64ad0302e210bede0dd0d7bf0
gayoungaa91 <gayoungaa91@gmail.com>
2020-02-06 18:15:28 +0900

    movie-app add files

--- study/React/movie-app/src/App.js
+++ study/React/movie-app/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}) {
+  console.log(fav)
+  return (
+    <div>
+      <h1>I like u {fav}</h1>
+      {/* <h2>{props.value}</h2> */}
+    </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Food 
+  fav  = "kimchi" />
 }
 
 export default App;
