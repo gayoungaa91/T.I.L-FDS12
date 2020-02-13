ad36b72132e8e18574b3f6998aa8c88e3bc89029
gayoungaa91 <gayoungaa91@gmail.com>
2020-02-13 21:21:23 +0900

    react/ex/todo

--- study/React/learning-react-book/exercise/todo-app/src/App.js
+++ study/React/learning-react-book/exercise/todo-app/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <div>렛츠고 TODO!!!!</div>
+  const [todos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트의 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱 만들어보기',
+      checked: false,
+    },
+  ]);
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos = {todos} />
+    </TodoTemplate>
+  )
 }
 
 export default App;
