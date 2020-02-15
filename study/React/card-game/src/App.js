5f92acfd764af0c32c859ae31c93323b2a917fb8
gayoungaa91 <gayoungaa91@gmail.com>
2020-02-15 23:59:39 +0900

    on

--- study/React/card-game/src/App.js
+++ study/React/card-game/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Card from './Card';
 import './App.css';
 
-function App() {
+const App = () => {
+   const [num, setNum] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
+   const [id, setId] = useState('');
+
   return (
     <div className="App">
-
+      <Card num = {num} setNum = {setNum} id = {id} setId = {setId} />
     </div>
   );
 }
