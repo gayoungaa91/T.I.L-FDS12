9fa2e0a4881af96d82a1f2b03f3a8f3c46f4542f
gayoungaa91 <gayoungaa91@gmail.com>
2020-02-29 23:33:41 +0900

    react/ex/01

---
+++ T.I.L/React/study-group/exercise/learn-redux/src/exercise.js
@@ -0,0 +1,76 @@
+import { createStore } from 'redux';
+
+// 상태 초기값 설정
+const initialState = {
+  counter: 0,
+  text: '',
+  list: []
+};
+
+const INCREASE = 'INCREASE'
+const DECREASE = 'DECREASE'
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+const increase = () => ({
+    type: INCREASE,
+  });
+
+const decrease = () => ({
+    type: DECREASE,
+  });
+
+  const chaneText = text => ({
+    type: CHANGE_TEXT,
+    text
+  })
+
+  const addToList = item => ({
+    type: ADD_TO_LIST,
+    item
+  })
+
+  function reducer(state = initialState, action) {
+    switch (action.type) {
+      case INCREASE:
+        return {
+          ...state,
+          counter: state.counter + 1
+        };
+      case DECREASE:
+        return {
+          ...state,
+          counter: state.counter + 1
+        };
+      case CHANGE_TEXT:
+        return {
+          ...state,
+          text: action.text
+        };
+      case ADD_TO_LIST:
+        return {
+          ...state,
+          list: state.list.concat(action.item)
+        };
+
+
+        default:
+          return state;
+    }
+  }
+
+  const store = createStore(reducer);
+  console.log(store.getState());
+
+  const listener = () => {
+    const state = store.getState();
+    console.log(state);
+  }
+
+  const unsubscribe = store.subscribe(listener);
+
+  store.dispatch(increase());
+  store.dispatch(decrease());
+  store.dispatch(chaneText('hello'));
+
+  window.store = store
