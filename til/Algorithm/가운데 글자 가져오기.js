c5d0c97ec451123ee86c3b0628999e3607942d01
gayoungaa91 <gayoungaa91@gmail.com>
2020-09-14 18:43:42 +0900

    til - modify algo

--- til/Algorithm/가운데 글자 가져오기.js
+++ til/Algorithm/가운데 글자 가져오기.js
@@ -1,27 +1,15 @@
-function solution(s) {
+function getMiddle(s) {
   let str =  s.split('')
-  // if(str.length % 2 === 1) {
-  //     let i = 0;
-  //     while( i < str.length - 1 ) {
-  //         str.length % 2 ? str.shift() : str.pop()
-  //     }
-  //     i++
-  // } else {
-  //     let j = 0;
-  //     while( j < 2) {
-  //         str.length % 2 ? str.pop() : str.shift()
-  //     }
-  //     j++
-  // }
   let i = 0;
-  while(i < str.length - 1) {
-      if(str.length % 2 !== 1) {
-           str.length % 2 ? str.shift() : str.pop()
-      } else {
-          str.length % 2 ? str.pop() : str.shift()
-      }
+  while(i < str.length -1) {
+    if(str.length % 2) {
+    str.shift() 
+    } else {
+      str.pop()
+    }
   }
-  i++
-  return str.toString() 
+  i++;
+
+  return str.join();
 }
 
