2cb368d893e2c301a93fcc8b23556789024fdac7
gayoungaa91 <gayoungaa91@gmail.com>
2020-09-16 16:01:13 +0900

    til - algo + 1

--- til/Algorithm/두 정수 사이의 합.js
+++ til/Algorithm/두 정수 사이의 합.js
@@ -5,3 +5,17 @@
 // a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
 // a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
 // a와 b의 대소관계는 정해져있지 않습니다.
+
+function solution(a, b) {
+  if (a === b) return a;
+
+  const min = a > b ? b : a;
+  const max = a > b ? a : b;
+
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
