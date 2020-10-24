92e9f7b2aca9e21ca29c7900436103192938101c
gayoungaa91 <gayoungaa91@gmail.com>
2020-10-24 23:27:37 +0900

    til

--- til/Javascript/exercise/todos/js/app.js
+++ til/Javascript/exercise/todos/js/app.js
@@ -3,3 +3,15 @@ let todos = [
   { id: 2, content: 'CSS', completed: true },
   { id: 3, content: 'Javascript', completed: false }
 ];
+
+const $input = document.querySelector('.input-todo');
+let $ul = document.querySelector('.todos');
+let checkBox = document.createElement('')
+let list = document.createElement('li');
+console.log($ul)
+$input.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter') {
+    $ul.innerHTML = $list
+    
+  }
+})
