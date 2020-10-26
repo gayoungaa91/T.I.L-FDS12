ad01b9b5aeca20e4dedfa53c2af0c48e8d42e49d
gayoungaa91 <gayoungaa91@gmail.com>
2020-10-26 21:13:44 +0900

    til/js

--- til/Javascript/exercise/todos/js/app.js
+++ til/Javascript/exercise/todos/js/app.js
@@ -6,12 +6,19 @@ let todos = [
 
 const $input = document.querySelector('.input-todo');
 let $ul = document.querySelector('.todos');
-let checkBox = document.createElement('')
+// let checkBox = document.createElement('')
 let list = document.createElement('li');
-console.log($ul)
+// console.log($ul)
 $input.addEventListener('keydown', (e) => {
-  if(e.key === 'Enter') {
-    $ul.innerHTML = $list
-    
-  }
+  // if(e.key === 'Enter') {
+  //   // $ul.innerHTML = `<li id="myId" class='todo-item'>{{$input.value}}</li>`
+  //   $ul.innerHTML = `<li id="myId" class="todo-item">
+  //   ${$input.value}
+  //   <input class="custom-checkbox" type="checkbox" id="ck-myId">
+  //     <label for="ck-myId"></label>
+  //     <i class="remove-todo far fa-times-circle"></i>
+  //     </li>`
+  // }
 })
+
+// const $nav = document.getElementsByTagName('li');
