fa648389a87da8f0eafb1d91386fcac43c8b391f
gayoungaa91 <gayoungaa91@gmail.com>
2021-05-07 15:28:09 +0900

    started basic

---
+++ til/Algorithm/course/basic/세가지 숫자중 최솟값.js
@@ -0,0 +1,20 @@
+// 100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하 세요.(정렬을 사용하면 안됩니다)
+
+// 1
+// function solution(A, B, C) {
+//   let arr = [A, B, C];
+//   arr.sort((a, b) => a - b);
+//   console.log(arr);
+// }
+
+// solution(6, 5, 1);
+
+// 2
+// function solution(A, B, C) {
+//   let answer;
+//   if (A < B) answer = A;
+//   else answer = B;
+//   if (answer > C) answer = C;
+//   return answer;
+// }
+// solution(6, 5, 11);
