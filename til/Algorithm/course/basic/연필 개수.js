7954c03886a3dd17702b6ad5556797953591abc9
gayoungaa91 <gayoungaa91@gmail.com>
2021-05-09 13:19:18 +0900

    add basic

--- til/Algorithm/course/basic/연필 개수.js
+++ til/Algorithm/course/basic/연필 개수.js
@@ -1,4 +1,19 @@
 // 연필 1 다스는 12자루입니다. 학생 1인당 연필을 1자루씩 나누어 준다고 할 때 N명이 학생수 를 입력하면 필요한 연필의 다스 수를 계산하는 프로그램을 작성하세요.
 
-function solution() {}
-solution();
+// 1.
+// function solution(n) {
+//   let dozen = Math.round(n / 12); // round는 틀림, 나머지가 무조건 잇으면 +1이 되어야함
+//   let remains = n % 12 === 0;
+
+//   if (!remains) {
+//     console.log(dozen + '다스');
+//   } else console.log(dozen + '다스');
+// }
+// solution(180);
+
+// 2
+function solution(n) {
+  let answer = Math.ceil(n / 12);
+  return answer;
+}
+console.log(solution(180));
