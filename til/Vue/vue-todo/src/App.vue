abe3071a44ea9620b6a5324b977a903dbfce2acf
gayoungaa91 <gayoungaa91@gmail.com>
2021-05-23 23:48:49 +0900

    til/vue

---
+++ til/Vue/vue-todo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+// let my_cmp = {
+//   template: '<div>my component</div>'
+// }
+// new Vue({
+//   // 컴포넌트 태그명 : 컴포넌트 내용
+//   el: '',
+//   components: {
+//      'my-cmp': my_cmp 
+//   }
+
+// });
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
